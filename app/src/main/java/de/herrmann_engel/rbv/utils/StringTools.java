ac3f33d78fbf0b18327e7f4e0f24f81b77cb9382
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2022-11-18 09:00:02 +0100

    improve list performance

--- app/src/main/java/de/herrmann_engel/rbv/utils/StringTools.java
+++ app/src/main/java/de/herrmann_engel/rbv/utils/StringTools.java
@@ -13,6 +13,11 @@ import java.util.regex.Pattern;
 
 public class StringTools {
 
+   private Pattern formatCardBoldItalicPattern;
+   private Pattern formatCardBoldPattern;
+   private Pattern formatCardItalicPattern;
+   private Pattern formatCardIUnderlinePattern;
+
     private int getTypeface(int i) {
         if (i == 3) {
             return Typeface.BOLD_ITALIC;
@@ -25,28 +30,57 @@ public class StringTools {
         }
     }
 
+    private Pattern generatePattern(int i) {
+        String regex =  String.format("%s%d%s%d%s", "(^|\\s|_)([*]{", i, "}[^\\s*][^\\n]*?(?<![\\s*])[*]{", i, "})");
+        return Pattern.compile(regex);
+    }
+
+    private Pattern getPattern(int i) {
+        if (i == 3) {
+            if(formatCardBoldItalicPattern == null){
+                formatCardBoldItalicPattern = generatePattern(i);
+            }
+            return formatCardBoldItalicPattern;
+        } else if (i == 2) {
+            if(formatCardBoldPattern == null){
+                formatCardBoldPattern = generatePattern(i);
+            }
+            return formatCardBoldPattern;
+        } else if (i == 1) {
+            if(formatCardItalicPattern == null){
+                formatCardItalicPattern = generatePattern(i);
+            }
+            return formatCardItalicPattern;
+        } else {
+            return null;
+        }
+    }
+
     public SpannableString format(String input) {
         SpannableStringBuilder output = new SpannableStringBuilder(input);
         StringBuffer modifiedInput = new StringBuffer(input);
         for (int i = 3; i > 0; i--) {
             int offset = 0;
-            String regex = String.format("%s%d%s%d%s", "(^|\\s|_)([*]{", i, "}[^\\s*][^\\n]*?(?<![\\s*])[*]{", i, "})");
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(modifiedInput);
-            while (matcher.find()) {
-                output.setSpan(new StyleSpan(getTypeface(i)), matcher.start(2) - offset, matcher.end(2) - offset, 0);
-                output.delete(matcher.start(2) - offset, matcher.start(2) - offset + i);
-                modifiedInput.delete(matcher.start(2) - offset, matcher.start(2) - offset + i);
-                offset += i;
-                output.delete(matcher.end(2) - offset - i, matcher.end(2) - offset);
-                modifiedInput.delete(matcher.end(2) - offset - i, matcher.end(2) - offset);
-                offset += i;
+            Pattern pattern = getPattern(i);
+            if(pattern != null) {
+                Matcher matcher = pattern.matcher(modifiedInput);
+                while (matcher.find()) {
+                    output.setSpan(new StyleSpan(getTypeface(i)), matcher.start(2) - offset, matcher.end(2) - offset, 0);
+                    output.delete(matcher.start(2) - offset, matcher.start(2) - offset + i);
+                    modifiedInput.delete(matcher.start(2) - offset, matcher.start(2) - offset + i);
+                    offset += i;
+                    output.delete(matcher.end(2) - offset - i, matcher.end(2) - offset);
+                    modifiedInput.delete(matcher.end(2) - offset - i, matcher.end(2) - offset);
+                    offset += i;
+                }
             }
         }
         int offset = 0;
-        String regex = "(^|\\s|[*])([_][^\\s_][^\\n]*?(?<![\\s_])[_])";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(modifiedInput);
+        if(formatCardIUnderlinePattern == null) {
+            String regex = "(^|\\s|[*])([_][^\\s_][^\\n]*?(?<![\\s_])[_])";
+            formatCardIUnderlinePattern = Pattern.compile(regex);
+        }
+        Matcher matcher = formatCardIUnderlinePattern.matcher(modifiedInput);
         while (matcher.find()) {
             output.setSpan(new UnderlineSpan(), matcher.start(2) - offset, matcher.end(2) - offset, 0);
             output.delete(matcher.start(2) - offset, matcher.start(2) - offset + 1);
