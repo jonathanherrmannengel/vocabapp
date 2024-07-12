5d7d5004839c9622608323c8eb27514d689bab09
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2024-07-12 19:03:46 +0200

    update dependencies and info

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion = "1.9.24"
+    val kotlinVersion = "2.0.0"
     extra.apply {
         set("kotlinVersion", kotlinVersion)
     }
@@ -9,7 +9,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("com.android.tools.build:gradle:8.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
