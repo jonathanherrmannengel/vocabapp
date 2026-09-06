2a44a869392c08a2acf35b5739320d56bef2f40a
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2026-09-06 05:59:25 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 dependencies {
     val kotlinVersion = rootProject.extra.get("kotlinVersion") as String
     val roomVersion = "2.8.4"
-    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
@@ -72,7 +72,7 @@ dependencies {
     implementation("io.noties.markwon:linkify:4.6.2")
     implementation("me.saket:better-link-movement-method:2.2.0")
     implementation("com.vanniktech:emoji-twitter:0.24.1")
-    implementation("io.coil-kt.coil3:coil:3.5.0")
+    implementation("io.coil-kt.coil3:coil:3.6.2")
     implementation("com.github.rtugeek:colorseekbar:2.1.0")
     implementation("com.atlassian.commonmark:commonmark:0.13.0")
     //match Markwon version of commonmark
